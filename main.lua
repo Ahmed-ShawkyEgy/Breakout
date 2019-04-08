f6c7d8955fcb8528d0ca71317b33c2bdcc671cee
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2019-04-08 19:30:29 +0200

    add score and health bar and add ServeState

--- main.lua
+++ main.lua
@@ -25,7 +25,8 @@ function love.load()
     gFrames = {
       ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
       ['balls'] = GenerateQuadsBalls(gTextures['main']),
-      ['bricks'] = GenerateQuadsBricks(gTextures['main'])
+      ['bricks'] = GenerateQuadsBricks(gTextures['main']),
+      ['hearts'] = GenerateQuads(gTextures['hearts'], 10, 9)
     }
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -53,6 +54,7 @@ function love.load()
 
     gStateMachine = StateMachine {
         ['start'] = function() return StartState() end,
+        ['serve'] = function() return ServeState() end,
         ['play'] = function() return PlayState() end
 
     }
@@ -103,4 +105,27 @@ function displayFPS()
     love.graphics.setFont(gFonts['small'])
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 5, 5)
+end
+
+function renderHealth(health)
+    -- start of our health rendering
+    local healthX = VIRTUAL_WIDTH - 100
+
+    -- render health left
+    for i = 1, health do
+        love.graphics.draw(gTextures['hearts'], gFrames['hearts'][1], healthX, 4)
+        healthX = healthX + 11
+    end
+
+    -- render missing health
+    for i = 1, 3 - health do
+        love.graphics.draw(gTextures['hearts'], gFrames['hearts'][2], healthX, 4)
+        healthX = healthX + 11
+    end
+end
+
+function renderScore(score)
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.print('Score:', VIRTUAL_WIDTH - 60, 5)
+    love.graphics.printf(tostring(score), VIRTUAL_WIDTH - 50, 5, 40, 'right')
 end
