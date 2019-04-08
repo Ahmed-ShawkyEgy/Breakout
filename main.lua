f5f3fba1fc0a453ca12e3113d9a24c3f6a48cf7f
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2019-04-08 17:34:55 +0200

    add paddle class

--- main.lua
+++ main.lua
@@ -50,7 +50,9 @@ function love.load()
     }
 
     gStateMachine = StateMachine {
-        ['start'] = function() return StartState() end
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end
+
     }
     gStateMachine:change('start')
 
